4136fe5a3da670440f99da3c73c30dfeba06d7ba
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2024-12-18 00:38:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-// Function expression to select elements `
-
+// Function expression to select elements
 const selectElement = (s) => document.querySelector(s);
 const navLinks = document.querySelectorAll(".nav-link");
 
+// Function to toggle the animation of nav links
+const toggleNavLinkAnimation = () => {
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinkAnimate 0.5s ease forwards ${index / 7 + 0.5}s`;
+            console.log(index / 7 + 0.5);
+        }
+    });
+};
+
+// Burger menu click event listener
 selectElement(".burger-menu-icon").addEventListener("click", () => {
     selectElement(".nav-list").classList.toggle("active");
-    selectElement(".burger-menu-icon").classList.toggle("toggle")
+    selectElement(".burger-menu-icon").classList.toggle("toggle");
 
-    navLinks.forEach((link, index) => {
-        if (link.style.animation){
-            link.style.animation = ""
-        }else{
-            link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
-            console.log(index/7 + 0.5)
-        }
-    })
+    toggleNavLinkAnimation();
 });
 
+// Nav link click event listener
 navLinks.forEach(link => {
     link.addEventListener("click", () => {
         selectElement(".nav-list").classList.toggle("active");
         selectElement(".burger-menu-icon").classList.toggle("toggle");
 
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = ""
-            }else{
-                link.style.animation = `navLinkAnimate 0.5s ease forwards ${ index/7 + 0.5}s`
-                console.log(index/7 + 0.5)
-            }
-        })
-    })
-})
+        toggleNavLinkAnimation();
+    });
+});
